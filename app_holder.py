ba5f4cf18bff33b7051a5684604b92e75c832918
maxwshen <maxwshen@gmail.com>
2020-06-08 18:31:10 -0400

    fix gtag, now in app_holder

--- app_holder.py
+++ app_holder.py
@@ -3,3 +3,30 @@ import dash
 app = dash.Dash(__name__)
 server = app.server
 app.config.suppress_callback_exceptions = True
+
+app.index_string = """<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-128953572-3"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-128953572-3');
+    </script>
+    {%metas%}
+    <title>{%title%}</title>
+    {%favicon%}
+    {%css%}
+  </head>
+  <body>
+    {%app_entry%}
+    <footer>
+      {%config%}
+      {%scripts%}
+      {%renderer%}
+    </footer>
+  </body>
+</html>"""
