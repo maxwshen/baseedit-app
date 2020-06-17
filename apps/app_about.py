05fb74074a4dfca261fdf4aab082cea310ae3dfc
maxwshen <maxwshen@gmail.com>
2020-06-17 18:11:22 -0400

    terms of use

--- apps/app_about.py
+++ apps/app_about.py
@@ -120,7 +120,42 @@ layout = html.Div([
               style = dict(
                 textAlign = 'center',
               )
-            )
+            ),
+
+            dcc.Markdown(dedent('''
+              Copyright 2020. The Broad Institute, Massachusetts Institute of Technology, and President and Fellows of Harvard College. All rights reserved, except those expressly identified below.
+
+              ***Limited License and Terms of Use for Research Use by Non-Profit and Government Institutions***
+              
+              As used in this limited license and terms of use, “Code” means this website, all software code running this website, all software code and models associated with this website, and all software code and models associated the following manuscript, no matter how the Code is accessed or obtained:
+
+              __Mandana Arbab\*, Max W. Shen\*__, Beverly Mok, Christopher Wilson, Żaneta Matuszek, Christopher A. Cassa, and David R. Liu. "Determinants of Base Editing Outcomes from Target Library Analysis and Machine Learning." _Cell_, 2020, in press.
+
+              BY DOWNLOADING OR USING THE CODE, YOU ARE CONSENTING TO BE AND AGREE TO BE BOUND BY ALL OF THE TERMS OF THIS LIMITED LICENSE, WHICH FOLLOW:
+
+              • You agree you will not use the Code, or any information derived from the Code, for any commercial purposes whatsoever.
+
+              • You agree you are an actively enrolled student, post-doctoral researcher, or faculty member at a degree-granting educational institution or US government research institution. 
+
+              • You agree you will only use the Code for educational, instructional, and/or non-commercial research purposes. 
+
+              • You understand that to obtain any right to use the Code for commercial purposes, including in the context of industrially sponsored research, you must enter into an appropriate separate and direct license agreement. For information on such licensing, please contact The Broad Institute using the following link: https://www.broadinstitute.org/contact 
+
+              • You agree you will not redistribute unmodified versions of the Code.
+
+              • You agree you will only modify the Code for educational, instructional, and/or non-commercial research purposes. 
+
+              • You agree that if you redistribute any modifications of the Code, you will redistribute those modifications under the same terms as this license and only to non-profits and US government institutions.
+
+              • For any use of the Code, including any modifications of the code, you agree you will credit the authors of the following manuscript: 
+
+               __Mandana Arbab\*, Max W. Shen\*__, Beverly Mok, Christopher Wilson, Żaneta Matuszek, Christopher A. Cassa, and David R. Liu. "Determinants of Base Editing Outcomes from Target Library Analysis and Machine Learning." _Cell_, 2020, in press.
+
+              • You agree to use neither the names of the owners (The Broad Institute, Massachusetts Institute of Technology (“MIT”), and President and Fellows of Harvard College (“Harvard”) or the names of the authors of the manuscript (above), to endorse or promote products or information derived from this software, without specific, prior written permission.
+
+              '''),
+              className = 'markdown_style',
+            ),
 
             # html.Div(
             #   [
